3489e4aae4963dcb74c74da884c7da2cf437d613
hediessid <essidhedi7000@gmail.com>
2017-07-24 22:18:51 -0500

    Create a HelloWorld App

---
+++ HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using InfrastructureHelloWorld;
+
+
+namespace HelloWorld.Controllers
+{
+    public class HelloWorldController : ApiController
+    {
+        public IEnumerable<ApiData> Get()
+        {
+            using (ProjectDBEntities entities = new ProjectDBEntities())
+            {
+                return entities.ApiDatas.ToList();
+            }
+        }
+
+        public ApiData Get(int id)
+        {
+            using (ProjectDBEntities entities = new ProjectDBEntities())
+            {
+                return entities.ApiDatas.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
---
+++ HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleApp
+{
+    class Program 
+    {
+        static HttpClient client = new HttpClient();
+
+        static void ShowMessage(HelloWorld msg)
+        {
+            Console.WriteLine($"Message: " + msg.Message);
+        }
+       
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+
+        static async Task<HelloWorld> GetMessageAsync(string path)
+        {
+            HelloWorld msg = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+               var HelloWorldJsonString = await response.Content.ReadAsStringAsync();
+                msg = JsonConvert.DeserializeObject<HelloWorld>(HelloWorldJsonString);
+                        
+            }
+            return msg;
+        }
+
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("http://localhost:50631/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            try
+            {
+                HelloWorld msg = await GetMessageAsync("api/HelloWorld/1");
+                ShowMessage(msg);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
